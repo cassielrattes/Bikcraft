56ce5bf1f3cecea6027d0c8cf77cbe7f74f9989d
Cassiel Rattes Cortez <cassielrattes@gmail.com>
2020-10-24 01:18:19 -0300

    ...

---
+++ js/script.js
@@ -0,0 +1,28 @@
+if (window.SimpleSlide) {
+  new SimpleSlide({
+    slide: "quote",
+    time: 5000,
+  });
+
+  new SimpleSlide({
+    slide: "portfolio",
+    time: 5000,
+    nav: true,
+  });
+}
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
+
+if (window.SimpleForm) {
+  new SimpleForm({
+    form: ".formphp",
+    button: "#enviar",
+    erro:
+      "<div id='form-erro'><h2>Erro no envio!</h2><p>Um erro ocorreu, tente para o email cassielrattes@gmail.com</p></div>",
+    sucesso:
+      "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu responderei</p></div>",
+  });
+}
+// SG.DyEwSVroTDmI4zyQ7ImZPQ.xdSFaqPBQNqUAnS3wIG - JtD_CYAH4EEJwfhFfWIgKGo;
